f3530c27103c6ec0877f9e1877882e94f872dd2d
HemlockK <2345serega5432@mail.ru>
2022-10-16 23:00:33 +0900

    homework complete

---
+++ task1#2/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+void number(int fNumber, int sNumber)
+{
+    double result = 1;
+    for (int newNumber = 1; newNumber <= sNumber; newNumber++)
+    {
+        result = result * fNumber;
+    }
+    Console.WriteLine(result);
+}
+
+number(125, 50);
---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.WriteLine("Введите цифры массива.Пример: 1, 12, ... 13, 4"); 
+string[] array = Console.ReadLine().Split(", ");
+if (array.Length != 8)
+{
+    Console.WriteLine("Данный массив не состоит из 8 символов");
+}
+int[] digital = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    digital[i] = Int32.Parse(array[i]);
+}
+Console.WriteLine("[{0}]", string.Join(", ", digital));
